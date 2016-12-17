05bde2d0cdfdc6535a1af7740953ae3fef57a46b
Milton Mazzarri <milmazz@gmail.com>
2016-12-16 21:29:52 -0600

    Add find/2 and more convenience functions for SubTags

--- lib/lang_tags/subtag.ex
+++ lib/lang_tags/subtag.ex
@@ -19,13 +19,55 @@ defmodule LangTags.SubTag do
   end
 
   @doc """
+  If found, returns a map for the given subtag, `nil` otherwise.
+  """
+  @spec find(String.t, String.t) :: map | nil
+  def find(subtag, type) do
+    try do
+      new(subtag, type)
+    rescue
+      RuntimeError -> nil
+    end
+  end
+
+  @doc """
   Get the subtag type
 
   See [RFC 5646 section 2.2](http://tools.ietf.org/html/rfc5646#section-2.2) for
   type definitions.
   """
   @spec type(map) :: String.t | nil
-  def type(subtag), do: subtag["Record"]["Type"]
+  def type(subtag) when is_map(subtag), do: subtag["Record"]["Type"]
+
+  @doc """
+  Returns `true` if subtag is of "language" type, `false` otherwise.
+  """
+  @spec language?(String.t) :: boolean
+  def language?(subtag), do: Registry.language?(subtag)
+
+  @doc """
+  Returns `true` if subtag is of "extlang" type, `false` otherwise.
+  """
+  @spec extlang?(String.t) :: boolean
+  def extlang?(subtag), do: Registry.extlang?(subtag)
+
+  @doc """
+  Returns `true` if subtag is of "script" type, `false` otherwise.
+  """
+  @spec script?(String.t) :: boolean
+  def script?(subtag), do: Registry.script?(subtag)
+
+  @doc """
+  Returns `true` if subtag is of "region" type, `false` otherwise.
+  """
+  @spec region?(String.t) :: boolean
+  def region?(subtag), do: Registry.region?(subtag)
+
+  @doc """
+  Returns `true` if subtag is of "variant" type, `false` otherwise.
+  """
+  @spec variant?(String.t) :: boolean
+  def variant?(subtag), do: Registry.variant?(subtag)
 
   @doc """
   Returns the list of description strings (a subtag may have more than one description)
@@ -37,7 +79,7 @@ defmodule LangTags.SubTag do
 
   """
   @spec descriptions(map) :: String.t | nil
-  def descriptions(subtag), do: subtag["Record"]["Description"]
+  def descriptions(subtag) when is_map(subtag), do: subtag["Record"]["Description"]
 
   @doc """
   Returns a preferred subtag as a map if the subtag is deprecated.
@@ -52,7 +94,7 @@ defmodule LangTags.SubTag do
 
   """
   @spec preferred(map) :: map | nil
-  def preferred(subtag) do
+  def preferred(subtag) when is_map(subtag) do
     process_preferred(subtag, subtag["Record"]["Preferred-Value"])
   end
 
@@ -70,14 +112,14 @@ defmodule LangTags.SubTag do
   for a definition of *Suppress-Script*.
   """
   @spec script(map) :: map | nil
-  def script(subtag) do
+  def script(subtag) when is_map(subtag) do
     script = subtag["Record"]["Suppress-Script"]
 
     if script, do: new(script, "script"), else: nil
   end
 
   @doc """
-  Returns the subtag scope as a string, or `nil` if the subtag has no scope.
+  Returns the subtag scope as a string, or "individual" if the subtag has no scope.
 
     ## Examples
 
@@ -87,8 +129,8 @@ defmodule LangTags.SubTag do
     "collection"
 
   """
-  @spec scope(map) :: String.t | nil
-  def scope(subtag), do: subtag["Record"]["Scope"]
+  @spec scope(map) :: String.t
+  def scope(subtag) when is_map(subtag), do: subtag["Record"]["Scope"] || "individual"
 
   @doc """
   Returns a date string reflecting the deprecation date if the subtag is deprecated, otherwise returns `nil`.
@@ -100,7 +142,7 @@ defmodule LangTags.SubTag do
 
   """
   @spec deprecated(map) :: String.t | nil
-  def deprecated(subtag), do: subtag["Record"]["Deprecated"]
+  def deprecated(subtag) when is_map(subtag), do: subtag["Record"]["Deprecated"]
 
   @doc """
   Returns a date string reflecting the date the subtag was added to the registry.
@@ -112,7 +154,7 @@ defmodule LangTags.SubTag do
 
   """
   @spec added(map) :: String.t | nil
-  def added(subtag), do: subtag["Record"]["Added"]
+  def added(subtag) when is_map(subtag), do: subtag["Record"]["Added"]
 
   @doc """
   Returns an list of comments, if any, otherwise returns an empty list.
@@ -124,7 +166,7 @@ defmodule LangTags.SubTag do
 
   """
   @spec comments(map) :: [String.t] | []
-  def comments(subtag), do: subtag["Record"]["Comments"] || []
+  def comments(subtag) when is_map(subtag), do: subtag["Record"]["Comments"] || []
 
   @doc """
   Return the subtag code formatted according to the case conventions defined in [RFC 5646 section 2.1.1](http://tools.ietf.org/html/rfc5646#section-2.1.1).
@@ -145,7 +187,7 @@ defmodule LangTags.SubTag do
 
   """
   @spec format(map) :: String.t
-  def format(subtag) do
+  def format(subtag) when is_map(subtag) do
     process_format(subtag["Subtag"], subtag["Record"]["Type"])
   end
 
@@ -173,7 +215,7 @@ defmodule LangTags.SubTag do
 
   """
   @spec collection?(String.t) :: boolean
-  def collection?(subtag), do: Registry.collection?(subtag)
+  def collection?(subtag) when is_binary(subtag), do: subtag |> String.downcase() |> Registry.collection?()
 
   @doc """
   Indicates if the given string is a macrolanguage as defined by ISO 639-3.
@@ -188,7 +230,7 @@ defmodule LangTags.SubTag do
 
   """
   @spec macrolanguage?(String.t) :: boolean
-  def macrolanguage?(subtag), do: Registry.macrolanguage?(subtag)
+  def macrolanguage?(subtag) when is_binary(subtag), do: subtag |> String.downcase() |> Registry.macrolanguage?()
 
   @doc """
   Indicates if the given string represents a special language code.
@@ -204,7 +246,7 @@ defmodule LangTags.SubTag do
 
   """
   @spec special?(String.t) :: boolean
-  def special?(subtag), do: Registry.special?(subtag)
+  def special?(subtag) when is_binary(subtag), do: subtag |> String.downcase() |> Registry.special?()
 
   @doc """
   Indicates if the given string represents a code reserved for private use in the ISO 639 standard.
@@ -219,5 +261,5 @@ defmodule LangTags.SubTag do
 
   """
   @spec private_use?(String.t) :: boolean
-  def private_use?(subtag), do: Registry.private_use?(subtag)
+  def private_use?(subtag) when is_binary(subtag), do: subtag |> String.downcase() |> Registry.private_use?()
 end
