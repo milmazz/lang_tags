2ba209bb0cac4d02d588fda2af7472092d5f29d5
Milton Mazzarri <milmazz@gmail.com>
2016-12-18 22:09:11 -0600

    Add basic information for hex.pm

--- mix.exs
+++ mix.exs
@@ -4,10 +4,12 @@ defmodule LangTags.Mixfile do
   def project do
     [app: :lang_tags,
      version: "0.1.0",
+     description: "Work with IANA language tags in Elixir (BCP47 / RFC5646)",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     package: package()]
   end
 
   def application do
@@ -18,5 +20,11 @@ defmodule LangTags.Mixfile do
     [{:credo, "~> 0.4", only: :dev},
      {:ex_doc, "~> 0.14", only: :dev},
      {:earmark, "~> 1.0", only: :dev}]
+  end
+
+  defp package do
+    [licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/milmazz/lang_tags",
+              "Docs" => "https://hexdocs.pm/lang_tag"}]
   end
 end
